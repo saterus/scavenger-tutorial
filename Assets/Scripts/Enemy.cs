24adb08b7c191b84c7557736c202e2d5fa449697
Alex Burkhart <saterus@gmail.com>
2017-02-21 15:43:54 -0500

    Alternate turns between Player & Enemies
    End of video 11.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -12,6 +12,7 @@ public class Enemy : MovingObject
 
 	protected override void Start ()
 	{
+		GameManager.instance.AddEnemyToList (this);
 		animator = GetComponent<Animator> ();
 		target = GameObject.FindGameObjectWithTag ("Player").transform;
 		base.Start ();
@@ -45,6 +46,7 @@ public class Enemy : MovingObject
 	protected override void OnCantMove<T> (T component)
 	{
 		Player hitPlayer = component as Player;
+		animator.SetTrigger ("enemyAttack");
 		hitPlayer.LoseFood (playerDamage);
 	}
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,9 +8,12 @@ public class GameManager : MonoBehaviour
 	public static GameManager instance = null;
 	public BoardManager boardScript;
 	public int playerFoodPoints = 100;
+	public float turnDelay = 0.1f;
 	[HideInInspector]public bool playersTurn = true;
 
 	private int level = 3;
+	private List<Enemy> enemies;
+	private bool enemiesMoving;
 
 	void Awake ()
 	{
@@ -21,17 +24,50 @@ public class GameManager : MonoBehaviour
 		}
 		DontDestroyOnLoad (gameObject);
 
+		enemies = new List<Enemy> ();
 		boardScript = GetComponent<BoardManager> ();
 		InitGame ();
 	}
 
 	void InitGame ()
 	{
+		enemies.Clear ();
 		boardScript.SetupScene (level);
 	}
 
 	public void GameOver ()
 	{
 		enabled = false;
+	}
+
+	void Update ()
+	{
+		if (playersTurn || enemiesMoving) {
+			return;
+		}
+
+		StartCoroutine (MoveEnemies ());
+	}
+
+	public void AddEnemyToList (Enemy enemy)
+	{
+		enemies.Add (enemy);
+	}
+
+	IEnumerator MoveEnemies ()
+	{
+		enemiesMoving = true;
+		yield return new WaitForSeconds (turnDelay);
+		if (enemies.Count == 0) {
+			yield return new WaitForSeconds (turnDelay);
+		} else {
+			foreach (var enemy in enemies) {
+				enemy.MoveEnemy ();
+				yield return new WaitForSeconds (enemy.moveTime);
+			}
+		}
+
+		playersTurn = true;
+		enemiesMoving = false;
 	}
 }
